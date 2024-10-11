d0a45017342e7e0184c819d4598ef1fd28107c1b
Seyf <seyf.bencheikh@gmail.com>
2024-10-11 11:34:54 +0200

    adding bat libraries

---
+++ lib.bat
@@ -0,0 +1,12 @@
+pip install robot.api.deco
+pip install openpyxl
+
+pip install json
+pip install requests
+
+pip install robotframework
+
+pip install robotframework-SeleniumLibrary
+pip install robotframework-JSONLibrary
+pip install robotframework-DateTime
+pip install robotframework-requests
