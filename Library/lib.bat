743a3fffa6fe02df480f86b3d18a97f509f6e903
testingAdib <seyf.bencheikh@gmail.com>
2024-10-11 19:07:19 +0200

    update lib file

--- Library/lib.bat
+++ Library/lib.bat
@@ -1,12 +1,9 @@
 pip install robot.api.deco
 pip install openpyxl
 
-pip install json
-pip install requests
-
 pip install robotframework
 
 pip install robotframework-SeleniumLibrary
 pip install robotframework-JSONLibrary
-pip install robotframework-DateTime
 pip install robotframework-requests
+pip install allure-robotframework
